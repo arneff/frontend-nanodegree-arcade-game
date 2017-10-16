d31b3771511b153039657173892801b5e994b669
Aaron Neff <arneff@gmail.com>
2017-10-16 08:54:00 -0600

    added functionality for user and enemy

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.xPosition = Math.random() * 184 + 50;
+    this.yPosition = Math.random() * 184 + 50;
+    this.speed = Math.random() * 256;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,21 +16,86 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.xPosition += this.speed * dt;
+
+    //place enemy at begining after reaching the end
+    if (this.xPosition >= 510) {
+      this.xPosition = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function(xPosition, yPosition) {
+  this.xPosition = 200;
+  this.yPosition = 400;
 
+
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/char-boy.png';
+
+};
+
+Player.prototype.update = function() {
+  inBounds(this);
+}
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
+
+}
+
+Player.prototype.handleInput = function(keyPress) {
+  if (keyPress === 'up') {
+    player.yPosition -= 25;
+  }
+  if (keyPress === 'down') {
+    player.yPosition += 25;
+  }
+  if (keyPress === 'left') {
+    player.xPosition -= 25;
+  }
+  if (keyPress === 'right') {
+    player.xPosition += 25;
+  }
+}
+
+let inBounds = function(player) {
+  if (player.yPosition > 400) {
+    player.yPosition = 400;
+  }
+  if (player.yPosition < -5) {
+    player.yPosition = -5;
+  }
+  if (player.xPosition > 410) {
+    player.xPosition = 410;
+  }
+  if (player.xPosition < -10) {
+    player.xPosition = -10;
+  }
+}
+
+let checkCollisions = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+let player = new Player();
+let enemy = new Enemy();
+
+allEnemies.push(enemy);
+
+
 
 
 
